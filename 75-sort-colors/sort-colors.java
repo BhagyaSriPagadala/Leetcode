51beea7a6604b16ed9894984a1b49e97bd2e6e28
BhagyaSriPagadala <159920657+BhagyaSriPagadala@users.noreply.github.com>
2025-06-10 09:45:35 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (32.90%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,44 +1,62 @@
+// class Solution {
+//     public void sortColors(int[] nums) {
+//         int n=nums.length;
+//         int z=0,o=0,t=0;
+//         for(int i=0;i<n;i++)
+//         {
+//             if(nums[i]==0)
+//             {
+//                 z++;
+//             }
+//             else if(nums[i]==1)
+//             {
+//                 o++;
+//             }
+//             else
+//             {
+//                 t++;
+//             }
+
+//         }
+//         int ind=0;
+//         for(int i=0;i<z;i++)
+//         {
+//             nums[ind++]=0;
+//         }
+//         for(int i=0;i<o;i++)
+//         {
+//             nums[ind++]=1;
+//         }
+//         for(int i=0;i<t;i++)
+//         {
+//             nums[ind++]=2;
+//         }
+//         for(int i=0;i<ind;i++)
+//         {
+//             System.out.println(nums);
+//         }
+//     }
+// }
 class Solution {
     public void sortColors(int[] nums) {
-        int n=nums.length;
-        int z=0,o=0,t=0;
-        for(int i=0;i<n;i++)
-        {
-            if(nums[i]==0)
-            {
-                z++;
+        int low = 0, mid = 0, high = nums.length - 1;
+        while (mid <= high) {
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            } else if (nums[mid] == 1) {
+                mid++;
+            } else {
+                swap(nums, mid, high);
+                high--;
             }
-            else if(nums[i]==1)
-            {
-                o++;
-            }
-            else
-            {
-                t++;
-            }
-
-        }
-        int ind=0;
-        for(int i=0;i<z;i++)
-        {
-            nums[ind++]=0;
-        }
-        for(int i=0;i<o;i++)
-        {
-            nums[ind++]=1;
-        }
-        for(int i=0;i<t;i++)
-        {
-            nums[ind++]=2;
-        }
-        for(int i=0;i<ind;i++)
-        {
-            System.out.println(nums);
         }
     }
-
-
-
-        
-
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 }
