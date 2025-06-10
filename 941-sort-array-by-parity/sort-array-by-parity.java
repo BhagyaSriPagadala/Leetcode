bcafff186fc7945ea84ef2fd48b31ed580641153
BhagyaSriPagadala <159920657+BhagyaSriPagadala@users.noreply.github.com>
2025-06-10 10:37:11 +0530

    Time: 1 ms (49.15%) | Memory: 45.1 MB (42.51%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.java
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -1,15 +1,34 @@
+// class Solution {
+//     public int[] sortArrayByParity(int[] nums) {
+//         int n = nums.length;
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = 0; j < n - 1; j++) {
+//                 if (nums[j] % 2 != 0 && nums[j + 1] % 2 == 0) {
+//                     int temp = nums[j];
+//                     nums[j] = nums[j + 1];
+//                     nums[j + 1] = temp;
+//                 }
+//             }
+//         }
+
+//         return nums;
+//     }
+// }
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int n = nums.length;
+        int left = 0, right = nums.length - 1;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n - 1; j++) {
-                if (nums[j] % 2 != 0 && nums[j + 1] % 2 == 0) {
-                    int temp = nums[j];
-                    nums[j] = nums[j + 1];
-                    nums[j + 1] = temp;
-                }
+        while (left < right) {
+            if (nums[left] % 2 > nums[right] % 2) {
+                // Swap if left is odd and right is even
+                int temp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = temp;
             }
+
+            if (nums[left] % 2 == 0) left++;
+            if (nums[right] % 2 == 1) right--;
         }
 
         return nums;
